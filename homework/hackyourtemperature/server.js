e6ebba29cd803498db1ae37e95d4e4816c201a6b
betysr <betysr96@gmail.com>
2022-10-03 14:18:16 +0200

    created post method

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,9 +1,17 @@
 import express from 'express';
 const app = express(); // express function creates new application
 
+app.use(express.json()); // is responsible for parsing the request body
+
 app.get("/", (req,res) => {
   res.setHeader("Content-type", "text/plain")
   res.send("hello from backend to frontend!");
+});
+
+app.post("/weather", (req,res) => {
+  const city = req.body.cityName;
+  res.setHeader("Content-type", "text/plain")
+  res.send(city);
 });
 
 app.listen(3000, () => {
